4c448105459a9eaa3b245f299fc46bd9a94b9abe
SebastianMorado <sabby.morado@gmail.com>
2022-01-29 15:30:33 +0800

    Initial Commit

---
+++ Slots SwiftUI/Slots_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Slots_SwiftUIApp.swift
+//  Slots SwiftUI
+//
+//  Created by Sebastian Morado on 1/29/22.
+//
+
+import SwiftUI
+
+@main
+struct Slots_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
